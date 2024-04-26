b9ecbec372ceedca65d78d179933cb0f38054fe7
Will Baxter <bax.will@googlemail.com>
2024-04-26 14:46:59 +0100

    Moved tweet form into its own component

--- src/Components/MainFeed.jsx
+++ src/Components/MainFeed.jsx
@@ -1,4 +1,5 @@
 import Tweets from "./Tweets";
+import TweetForm from "./TweetForm"
 
 export default function MainFeed({
   loggedInUser,
@@ -7,21 +8,7 @@ export default function MainFeed({
   tweets,
   setTweets,
 }) {
-  const addTweet = (e) => {
-    e.preventDefault();
-    setTweets([
-      {
-        ...loggedInUser,
-        date: "1m",
-        content: createTweetContent,
-        commentCount: 0,
-        retweetCount: 0,
-        heartCount: 0,
-        analyticsCount: 0,
-      },
-      ...tweets,
-    ]);
-  };
+
 
   return (
     <main>
@@ -30,43 +17,7 @@ export default function MainFeed({
       </div>
 
       <div className="create-tweet">
-        <form onSubmit={addTweet}>
-          <div className="avatar-section">
-            <div className="profile-icon">
-              <img src={loggedInUser.profileImage} />
-            </div>
-          </div>
-
-          <div className="textarea-section">
-            <textarea
-              className="content"
-              type="text"
-              placeholder="What is happening?!"
-              value={createTweetContent}
-              onChange={(e) => setCreateTweetContent(e.target.value)}
-            ></textarea>
-          </div>
-
-          <div></div>
-
-          <div className="actions-section">
-            <div className="actions">
-              <i className="fa-regular fa-image action-icon"></i>
-              <i className="fa-solid fa-list action-icon"></i>
-              <i className="fa-regular fa-face-smile action-icon"></i>
-              <i className="fa-regular fa-calendar action-icon"></i>
-              <i className="fa-solid fa-location-dot action-icon"></i>
-            </div>
-
-            <button
-              type="submit"
-              disabled={createTweetContent.length < 1}
-              className="tweet-btn"
-            >
-              Tweet
-            </button>
-          </div>
-        </form>
+        <TweetForm loggedInUser={loggedInUser} createTweetContent={createTweetContent} setCreateTweetContent={setCreateTweetContent} setTweets={setTweets} tweets={tweets}/>
       </div>
 
       <div className="show-more-tweets">
