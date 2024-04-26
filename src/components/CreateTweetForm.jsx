f114c9f57e6a657ce5d06427e7ceea99b8a18e5b
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-04-26 12:16:33 +0200

    add actions into seperate function

--- src/components/CreateTweetForm.jsx
+++ src/components/CreateTweetForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Actions from './Actions'
 
 function CreateTweetForm({ loggedInUser, setTweets, tweets }) {
     const [createTweetContent, setCreateTweetContent] = useState('')
@@ -38,13 +39,7 @@ function CreateTweetForm({ loggedInUser, setTweets, tweets }) {
             <div></div>
 
             <div className="actions-section">
-                <div className="actions">
-                    <i className="fa-regular fa-image action-icon"></i>
-                    <i className="fa-solid fa-list action-icon"></i>
-                    <i className="fa-regular fa-face-smile action-icon"></i>
-                    <i className="fa-regular fa-calendar action-icon"></i>
-                    <i className="fa-solid fa-location-dot action-icon"></i>
-                </div>
+                <Actions />
 
                 <button type="submit" disabled={createTweetContent.length < 1} className="tweet-btn">Tweet</button>
             </div>
