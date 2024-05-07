75fb18ecc261f1af51ba5269668e7e65f0351166
timsakande <timiakande64@gmail.com>
2024-05-07 21:08:26 +0100

    fixed issues

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const LeftSidebar = ({ loggedInUser }) => {
   return (
+
+     
     <aside className="left-side">
+
+      
+      {/* ... */}
+     
+
+
       <div className="logo">
         <i className="fa-brands fa-twitter"></i>
       </div>
@@ -14,7 +22,65 @@ const LeftSidebar = ({ loggedInUser }) => {
         </a>
       </div>
 
-      {/* Rest of the menu items */}
+      <div className="menu-item">
+        <a href="#">
+          <i className="fa-solid fa-magnifying-glass"></i>
+          Explore
+        </a>
+      </div>
+
+      <div className="menu-item">
+        <a href="#">
+          <i className="fa-regular fa-bell"></i>
+          Notifications
+        </a>
+      </div>
+
+      <div className="menu-item">
+        <a href="#">
+          <i className="fa-regular fa-envelope"></i>
+          Messages
+        </a>
+      </div>
+
+      <div className="menu-item">
+        <a href="#">
+          <i className="fa-solid fa-bars"></i>
+          Lists
+        </a>
+      </div>
+
+      <div className="menu-item">
+        <a href="#">
+          <i className="fa-regular fa-bookmark"></i>
+          Bookmarks
+        </a>
+      </div>
+
+      <div className="menu-item">
+        <a href="#">
+          <i className="fa-regular fa-circle-check"></i>
+          Verified
+        </a>
+      </div>
+
+      <div className="menu-item">
+        <a href="#">
+          <i className="fa-regular fa-user"></i>
+          Profile
+        </a>
+      </div>
+
+      <div className="menu-item">
+        <a href="#">
+          <i className="fa-solid fa-ellipsis"></i>
+          More
+        </a>
+      </div>
+
+      
+
+      {/* ... */}
 
       <button className="tweet-btn">Tweet</button>
 
@@ -30,8 +96,8 @@ const LeftSidebar = ({ loggedInUser }) => {
 
         <div className="action">
           <i className="fa-solid fa-ellipsis"></i>
+        </div> 
         </div>
-      </div>
     </aside>
   );
 };
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -6,11 +6,42 @@ const MainContent = ({ loggedInUser, tweets, createTweetContent, setCreateTweetC
     <main>
       <div className="top-bar">
         <h2 className="title">Home</h2>
+        <div className="icon">
+          <i className="fa-solid fa-dog"></i>
+        </div>
       </div>
 
       <div className="create-tweet">
         <form onSubmit={addTweet}>
-          {/* Create tweet form */}
+          <div className="avatar-section">
+            <div className="profile-icon">
+              <img src={loggedInUser.profileImage} alt="Profile" />
+            </div>
+          </div>
+
+          <div className="textarea-section">
+            <textarea
+              className="content"
+              type="text"
+              placeholder="What is happening?!"
+              value={createTweetContent}
+              onChange={(e) => setCreateTweetContent(e.target.value)}
+            ></textarea>
+          </div>
+
+          <div className="actions-section">
+            <div className="actions">
+              <i className="fa-regular fa-image action-icon"></i>
+              <i className="fa-solid fa-list action-icon"></i>
+              <i className="fa-regular fa-face-smile action-icon"></i>
+              <i className="fa-regular fa-calendar action-icon"></i>
+              <i className="fa-solid fa-location-dot action-icon"></i>
+            </div>
+
+            <button type="submit" disabled={createTweetContent.length < 1} className="tweet-btn">
+              Tweet
+            </button>
+          </div>
         </form>
       </div>
 
