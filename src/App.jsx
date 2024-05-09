0ff48fed7cf08c777882b3ecfde5363c938e33d5
samisalehsaeed <samisalehsaeed05@gmail.com>
2024-05-10 00:52:09 +0100

    Sami Saeed

--- src/App.jsx
+++ src/App.jsx
@@ -51,6 +51,7 @@ function App() {
           loggedInUser={loggedInUser}
           createTweetContent={createTweetContent}
           addTweet={addTweet}
+          setCreateTweetContent={setCreateTweetContent}
         />
         <ShowMore />
         <TweetContent tweets={tweets} />
--- src/Components/CreateTweet.jsx
+++ src/Components/CreateTweet.jsx
@@ -1,6 +1,7 @@
 export default function CreateTweet({
   loggedInUser,
   createTweetContent,
+  setCreateTweetContent,
   addTweet,
 }) {
   return (
