b8441a3a4bf1bd4a9f0e4446f9ba188ff97182f9
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-06-25 17:24:17 +0100

    Completed core + extension 3

---
+++ src/Main-feed.jsx
@@ -0,0 +1,55 @@
+import RenderTweets from "./Render-tweets";
+export default function MainFeed(props) {
+  return (
+    <main>
+      <div className="top-bar">
+        <h2 className="title">Home</h2>
+      </div>
+
+      <div className="create-tweet">
+        <form onSubmit={props.addTweet}>
+          <div className="avatar-section">
+            <div className="profile-icon">
+              <img src={props.loggedInUser.profileImage} />
+            </div>
+          </div>
+
+          <div className="textarea-section">
+            <textarea
+              className="content"
+              type="text"
+              placeholder="What is happening?!"
+              value={props.createTweetContent}
+              onChange={(e) => props.setCreateTweetContent(e.target.value)}
+            ></textarea>
+          </div>
+
+          <div></div>
+
+          <div className="actions-section">
+            <div className="actions">
+              <i className="fa-regular fa-image action-icon"></i>
+              <i className="fa-solid fa-list action-icon"></i>
+              <i className="fa-regular fa-face-smile action-icon"></i>
+              <i className="fa-regular fa-calendar action-icon"></i>
+              <i className="fa-solid fa-location-dot action-icon"></i>
+            </div>
+
+            <button
+              type="submit"
+              disabled={props.createTweetContent.length < 1}
+              className="tweet-btn"
+            >
+              Tweet
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div className="show-more-tweets">
+        <p>Show 35 Tweets</p>
+      </div>
+      <RenderTweets tweets = {props.tweets} searchTweets ={props.searchTweets}/>
+    </main>
+  );
+}
